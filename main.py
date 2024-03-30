c80b7b3022da9095a8e07519941dc6122a9f5708
Muhammad Ahsan <138124122+muhammadAhsanDS@users.noreply.github.com>
2024-03-31 02:41:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import pickle
 
 # Load data
 data = pd.read_csv('spam.csv', encoding='ISO-8859-1')
-data = data.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'],axis=1)
+data = data.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
 data['v1'] = data['v1'].map({'ham': 0, 'spam': 1})
+
 # Split data into features and labels
 x = data['v2']
 y = data['v1']
+
 vectorizer = TfidfVectorizer()
 x_vectorized = vectorizer.fit_transform(x)
+
 model = MultinomialNB()
 model.fit(x_vectorized, y)
+
 pickle.dump(model, open('model.pkl', 'wb'))
 print('Model saved as model.pkl')
